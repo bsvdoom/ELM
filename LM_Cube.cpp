90513f2df5df67d5c73a90d452482e82c8a0a39f
adam <flowtex@gmail.com>
2026-02-09 02:36:43 +0100

    more detections

--- LM_Cube.cpp
+++ LM_Cube.cpp
@@ -66,14 +66,78 @@ void LM_Cube::getTopSide(float roll, float pitch) {
 
   topSideRaw = currentSideUnmapped - 1;
 
-  mapSides();
-
 }
 
 
-void LM_Cube::mapSides() {
-  topSide = sideRemap[topSideRaw];
-} 
+CubeOrientation LM_Cube::getFullOrientation() {
+  CubeOrientation o;
+
+  uint8_t t = topSideRaw;
+
+  o.top    = cubeMap[t][0];
+  o.bottom = cubeMap[t][1];
+  o.front  = cubeMap[t][2];
+  o.back   = cubeMap[t][3];
+  o.left   = cubeMap[t][4];
+  o.right  = cubeMap[t][5];
+
+  return o;
+}
+
+
+void LM_Cube::updateStability(float roll, float pitch) {
+
+  float tilt = sqrt(roll * roll + pitch * pitch);
+
+  isStable = false;
+  balancesOnEdge = false;
+  balancesOnCorner = false;
+
+  if (tilt < 15) {
+    isStable = true;
+  }
+  else if (tilt >= 30 && tilt < 60) {
+    balancesOnEdge = true;
+  }
+  else if (tilt >= 60) {
+    balancesOnCorner = true;
+  }
+}
+
+
+float LM_Cube::getRotationSpeed(float gyroX, float gyroY, float gyroZ) {
+  return sqrt(gyroX*gyroX + gyroY*gyroY + gyroZ*gyroZ);
+}
+
+void LM_Cube::updateRotationState(float gyroX, float gyroY, float gyroZ) {
+
+  float absX = abs(gyroX);
+  float absY = abs(gyroY);
+  float absZ = abs(gyroZ);
+
+  rotationSpeed = getRotationSpeed(gyroX, gyroY, gyroZ);
+
+  if (rotationSpeed < 10) {
+    rotationAxis = 0; // nincs forgás
+    rotationDirection = 0;
+    return;
+  }
+
+  if (absX > absY && absX > absZ) {
+    rotationAxis = 1; // X tengely
+    rotationDirection = (gyroX > 0) ? 1 : -1;
+  }
+  else if (absY > absZ) {
+    rotationAxis = 2; // Y tengely
+    rotationDirection = (gyroY > 0) ? 1 : -1;
+  }
+  else {
+    rotationAxis = 3; // Z tengely
+    rotationDirection = (gyroZ > 0) ? 1 : -1;
+  }
+}
+
+
 
 
 void LM_Cube::handleFlip() {
--- LM_Cube.h
+++ LM_Cube.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Arduino.h"
 
-#define DEFAULT_SIDE_REMAP {5, 4, 3, 2, 1, 0}
+struct CubeOrientation {
+  uint8_t top;
+  uint8_t bottom;
+  uint8_t front;
+  uint8_t back;
+  uint8_t left;
+  uint8_t right;
+};
+
 
 class LM_Cube
 {
@@ -21,7 +29,6 @@ public:
   void handleFlip();
   void update_leds(uint8_t topSide);
 
-  int8_t sideRemap[6] = DEFAULT_SIDE_REMAP;
   bool apMode=false;
   uint8_t currentSideUnmapped = 1;
   uint8_t currentSide = 1;
@@ -37,4 +44,40 @@ public:
 
   uint8_t topSideRaw = 0;
   uint8_t topSide = 0;
+
+
+  // index: topSideRaw (0–5)
+  // sorrend: {top, bottom, front, back, left, right}
+  const uint8_t cubeMap[6][6] = {
+    // top = 0
+    {0, 5, 1, 4, 2, 3},
+
+    // top = 1
+    {1, 4, 5, 0, 2, 3},
+
+    // top = 2
+    {2, 3, 1, 4, 5, 0},
+
+    // top = 3
+    {3, 2, 1, 4, 0, 5},
+
+    // top = 4
+    {4, 1, 0, 5, 2, 3},
+
+    // top = 5
+    {5, 0, 1, 4, 3, 2}
+  };
+
+  CubeOrientation getFullOrientation();
+  void updateStability(float roll, float pitch);
+  float getRotationSpeed(float gyroX, float gyroY, float gyroZ);
+
+
+  void updateRotationState(float gyroX, float gyroY, float gyroZ);
+  uint8_t rotationAxis;
+  uint8_t rotationDirection;
+  uint8_t rotationSpeed; 
+
+  bool isStable, balancesOnEdge, balancesOnCorner;
+
 };
